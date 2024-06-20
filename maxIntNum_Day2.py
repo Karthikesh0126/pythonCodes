02406450a265a32ee18b7863c796c7c2bf4bc02e
Karthikesh Chary <157310051+Karthikesh0126@users.noreply.github.com>
2024-06-20 19:10:23 +0530

    Add files via upload

---
+++ maxIntNum_Day2.py
@@ -0,0 +1,2 @@
+nums = list(map(int, input("Enter the list of integers: ").split()))
+print("The Maximum number in the list is:",max(nums)) #finding the maximum number using max() function
---
+++ stringRevDay2.py
@@ -0,0 +1,2 @@
+string = input("Enter the string: ")
+print("The Reversed String is:",string[::-1]) #using slicing feature
